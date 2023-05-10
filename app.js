92b33c39671b3680f36dc2e6a91b600a3e2cf743
novaa0911 <novanurachman98@gmail.com>
2023-05-10 08:23:32 +0700

    App

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+
+const biodata = require("./modulBiodata");
+
+console.log("Nama: " + biodata.getNama());
+console.log("Tempat Lahir: " + biodata.getTempatLahir());
+console.log("Tanggal Lahir: " + biodata.getTanggalLahir());
+console.log("Alamat: " + biodata.getAlamat());
